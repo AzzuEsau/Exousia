5eedcbb3fa5d9412e26096843c8e116ad6db1286
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2022-10-19 01:40:57 -0500

    CineMachine and system of money implemented

---
+++ Assets/Scripts/Entity/Interactable/Interactable.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+
+   public virtual void Interact()
+   {
+        Debug.Log("Interactuando con alg�n objeto");
+   }
+
+}
