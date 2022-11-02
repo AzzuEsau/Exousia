9c4bf973fed17a9996760a88b5a363b7328b81a4
Hector Guerrero Diaz <hjesus.guerrero@alumnos.udg.mx>
2022-11-02 12:29:36 -0600

    comments eliminated

--- Assets/Scripts/GameManager/GameManager.cs
+++ Assets/Scripts/GameManager/GameManager.cs
@@ -5,10 +5,6 @@ using UnityEngine;
 public class GameManager : GenericSingletonClass<GameManager>
 {
 
-    // private UI_Manager _ui_Manager;
-    // private PlayerController _playerData;
-    // private SaveLoadManager _saveLoadManager;
-    // private NPCManager _NPCManager;
     private DecisionController decisionController;
 
     private string _notFoundMessage = "El manejador de juego no encontro un ";
@@ -18,10 +14,6 @@ public class GameManager : GenericSingletonClass<GameManager>
     void Start()
     {
         decisionController = FindDecisionController();
-        // _ui_Manager = FindUI_Manager();
-        // _playerData = FindPlayerData();
-        // _saveLoadManager = FindSaveLoadManager();
-        // _NPCManager = FindNPCManager();
     }
 
     public DecisionController FindDecisionController()
@@ -35,48 +27,6 @@ public class GameManager : GenericSingletonClass<GameManager>
         return component;
     }
 
-
-    // public UI_Manager FindUI_Manager()
-    // {
-    //     UI_Manager var = FindObjectOfType<UI_Manager>();
-    //     string compName = "UI Manager";
-    //     if(var==null)
-    //     {
-    //         Debug.LogWarning(_notFoundMessage + compName);
-    //     }
-    //     return var;
-    // }
-    // public PlayerController FindPlayerData()
-    // {
-    //     PlayerController var = FindObjectOfType<PlayerController>();
-    //     string compName = "PlayerData";
-    //     if (var == null)
-    //     {
-    //         Debug.LogWarning(_notFoundMessage + compName);
-    //     }
-    //     return var;
-    // }
-    // public SaveLoadManager FindSaveLoadManager()
-    // {
-    //     SaveLoadManager var = FindObjectOfType<SaveLoadManager>();
-    //     string compName = "SaveLoadManager";
-    //     if (var == null)
-    //     {
-    //         Debug.LogWarning(_notFoundMessage + compName);
-    //     }
-    //     return var;
-    // }
-    // public NPCManager FindNPCManager()
-    // {
-    //     NPCManager var = FindObjectOfType<NPCManager>();
-    //     string compName = "NPCManager";
-    //     if (var == null)
-    //     {
-    //         Debug.LogWarning(_notFoundMessage + compName);
-    //     }
-    //     return var;
-    // }
-
     public DecisionController GetDecisionController()
     {
         DecisionController compVar = decisionController;
@@ -88,44 +38,4 @@ public class GameManager : GenericSingletonClass<GameManager>
         return compVar;
     }
 
-    // public UI_Manager GetUI_Manager()
-    // {
-    //     UI_Manager compVar = _ui_Manager;
-    //     string compName = "UI Manager";
-    //     if (compVar == null)
-    //     {
-    //         Debug.LogWarning(_notGotMessage + compName);
-    //     }
-    //     return compVar;
-    // }
-    // public PlayerController GetPlayerData()
-    // {
-    //     PlayerController compVar = _playerData;
-    //     string compName = "PlayerData";
-    //     if (compVar == null)
-    //     {
-    //         Debug.LogWarning(_notGotMessage + compName);
-    //     }
-    //     return compVar;
-    // }
-    // public SaveLoadManager GetSaveLoadManager()
-    // {
-    //     SaveLoadManager compVar = _saveLoadManager;
-    //     string compName = "SaveLoadManager";
-    //     if (compVar == null)
-    //     {
-    //         Debug.LogWarning(_notGotMessage + compName);
-    //     }
-    //     return compVar;
-    // }
-    // public NPCManager GetNPCManager()
-    // {
-    //     NPCManager compVar = _NPCManager;
-    //     string compName = "NPCManager";
-    //     if (compVar == null)
-    //     {
-    //         Debug.LogWarning(_notGotMessage + compName);
-    //     }
-    //     return compVar;
-    // }
 }
